497aeade6d698246e2c74652369108c3dfcfb7cd
Benjamin Kanzler <github@kanzler-online.de>
2025-11-14 20:17:53 +0100

    feature: using flask test as cli to run tests in app_context

--- llm-api-connector.py
+++ llm-api-connector.py
@@ -40,3 +40,10 @@ def setup_logging():
 
 setup_logging()
 app = create_app()
+
+@app.cli.command("test")
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
